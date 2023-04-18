5efb63b83a16de1cc949a28fbba4385605b90a0b
fakulencinas07 <fakulencinas@gmail.com>
2023-04-18 19:48:11 -0300

    v5

--- js/main.js
+++ js/main.js
@@ -8,10 +8,17 @@ class Cucha {
     }
 }
 
+<<<<<<< HEAD
 const gatiCueva = new Cucha(1, "Gati Cueva", 6000, "../images/Cuchas-Regaleria/cucha1.png");
 const gatiLoft = new Cucha(2, "Mini Gati", 20000, "../images/Cuchas-Regaleria/cucha2.png");
 const miniGati = new Cucha(3, "Gati Loft", 10000, "../images/Cuchas-Regaleria/cucha55.png");
 const gatiSuite = new Cucha(4, "Gati Suite", 54000, "../images/Cuchas-Regaleria/cucha4.png")
+=======
+const gatiCueva = new Cucha(1, "Gati Cueva", 6000, "../images/Cuchas-Regaleria/cucha1.jpeg");
+const gatiLoft = new Cucha(2, "Gati Loft", 10000, "../images/Cuchas-Regaleria/cucha2.jpeg");
+const miniGati = new Cucha(3, "Mini Gati", 20000, "../images/Cuchas-Regaleria/cucha55.jpeg");
+const gatiSuite = new Cucha(4, "Gati Suite", 54000, "../images/Cuchas-Regaleria/cucha4.jpeg")
+>>>>>>> 2108b86657e777ecd2c5510490a63366ad8a0b85
 
 const cuchas = [gatiCueva, gatiLoft, miniGati, gatiSuite];
 
@@ -32,7 +39,11 @@ const mostrarCuchas = () => {
                     <img src="${Cucha.img}" class= "card-img-tom imgCuchas" alt="cucha">
                     <div class="card-body" >
                     <p>${Cucha.nombre}</p>
+<<<<<<< HEAD
                     <span>$${Cucha.precio}</span>
+=======
+                    <span>${Cucha.precio}</span>
+>>>>>>> 2108b86657e777ecd2c5510490a63366ad8a0b85
                         <button class = "btn colorBoton" id= "boton${Cucha.id}" >Agregar al carrito</button>
                 </div>
             </div>`
@@ -85,7 +96,11 @@ const mostrarCarrito = () => {
                     <div class="card-body" >
                     <p>${Cucha.nombre}</p>
                     <p>${Cucha.cantidad}</p>
+<<<<<<< HEAD
                     <span>$${Cucha.precio}</span>
+=======
+                    <span>${Cucha.precio}</span>
+>>>>>>> 2108b86657e777ecd2c5510490a63366ad8a0b85
                         <button class = "btn colorBoton" id="eliminar${Cucha.id}" >Eliminar del carrito</button>
                 </div>
             </div>`
