fbf0af20f01bef8bef360a823ddbae01d41faf02
fakulencinas07 <fakulencinas@gmail.com>
2023-04-20 13:31:10 -0300

    arreglando errores

---
+++ js/form.js
@@ -0,0 +1,19 @@
+const $form = document.getElementById("form");
+
+$form.addEventListener('submit', handleSubmit)
+
+async function handleSubmit(event){
+    event.preventDefault()
+    const form = new FormData(this)
+    const response = await fetch(this.action, {
+        method: this.method,
+        body: form,
+        headers:{
+        'accept': 'application/json'
+        }
+    } )
+    if(response.ok){
+        this.reset()
+        Swal.fire('Gracias, nos contactaremos pronto con vos!')
+    }
+}
